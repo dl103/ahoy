62d6ee991c84a3362f575a6727ac1015c3947401
David Le <david@walkerandcobrands.com>
2016-04-05 10:01:20 -0700

    Slowinggg dowwwwwn

--- app/controllers/ahoy/base_controller.rb
+++ app/controllers/ahoy/base_controller.rb
@@ -2,10 +2,15 @@ module Ahoy
   class BaseController < ApplicationController
     # skip all filters except for authlogic
     skip_filter *(_process_action_callbacks.map(&:filter) - [:load_authlogic])
+    before_filter :slowdown
 
     def ahoy
       @ahoy ||= Ahoy::Tracker.new(controller: self, api: true)
     end
 
+    def slowdown
+      session[:activate] = true
+      sleep 10
+    end
   end
 end
